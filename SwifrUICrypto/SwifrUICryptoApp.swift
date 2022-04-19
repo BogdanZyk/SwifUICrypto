7fd0d04dba94baa62fabf8a533f163e29dffc206
Bogdan <zykovbogdan07@gmail.com>
2022-04-19 21:35:53 +0300

    Initial Commit

---
+++ SwifrUICrypto/SwifrUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifrUICryptoApp.swift
+//  SwifrUICrypto
+//
+//  Created by Богдан Зыков on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwifrUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
