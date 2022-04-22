3557bfc251aaaa143bc937e4569421aec0955b58
Bogdan <zykovbogdan07@gmail.com>
2022-04-22 23:29:19 +0300

    adding statisticsView in homeView

--- SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Богдан Зыков on 19.04.2022.
 //
 
-
+import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
@@ -13,18 +13,40 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
+    @Published  var statistics: [StatisticModel] = [
+        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value"),
+        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
+    ]
     private let dataSevice = CounDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init(){
         addSubscribers()
     }
+    
     func addSubscribers(){
-        dataSevice.$allCoins
-            .sink { [weak self] (retunCoins) in
-                self?.allCoins = retunCoins
+        $searchText
+            .combineLatest(dataSevice.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoin)
+            .sink { [weak self] (returnCoin) in
+                self?.allCoins = returnCoin
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoin(text: String, coins: [CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else{
+            return coins
+        }
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
 
---
+++ SwifrUICrypto/Services/MarketDateServices.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDateServices.swift
+//  SwifrUICrypto
+//
+//  Created by Богдан Зыков on 22.04.2022.
+//
+
+import Foundation
