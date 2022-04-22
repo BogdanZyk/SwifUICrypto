c2650df9b82e4f4327a47d872b63478b0a213d77
Bogdan <zykovbogdan07@gmail.com>
2022-04-22 23:58:12 +0300

    adding mapDataService

--- SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,13 +13,9 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
-    @Published  var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
-    ]
-    private let dataSevice = CounDataService()
+    @Published  var statistics: [StatisticModel] = []
+    private let coinDataService = CounDataService()
+    private let marketDataService = MarketDataServices()
     private var cancellables = Set<AnyCancellable>()
     
     init(){
@@ -28,11 +24,17 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers(){
         $searchText
-            .combineLatest(dataSevice.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoin)
             .sink { [weak self] (returnCoin) in
                 self?.allCoins = returnCoin
+            }
+            .store(in: &cancellables)
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnStats) in
+                self?.statistics = returnStats
             }
             .store(in: &cancellables)
     }
@@ -47,6 +49,20 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel]{
+        var stats: [StatisticModel] = []
+        guard let data = marketDataModel else{
+            return stats
+        }
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h volume", value: data.volume)
+        let btcDominans = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio value", value: "$0.00", percentageChange: 0)
+        stats.append(contentsOf: [
+            marketCap,volume, btcDominans, portfolio
+        ])
+        return stats
     }
 }
 
--- SwifrUICrypto/Services/MarketDateServices.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MarketDateServices.swift
-//  SwifrUICrypto
-//
-//  Created by Богдан Зыков on 22.04.2022.
-//
-
-import Foundation
