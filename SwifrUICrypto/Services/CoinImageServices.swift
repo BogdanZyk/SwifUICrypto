683e4c6c06eef90e11a5fa254f4f6a9bf7028305
Bogdan <zykovbogdan07@gmail.com>
2022-04-22 21:56:00 +0300

    change download image func save in cache

--- SwifrUICrypto/Services/CoinImageServices.swift
+++ SwifrUICrypto/Services/CoinImageServices.swift
@@ -14,21 +14,35 @@ class CoinImageServices{
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     init(coin: CoinModel){
         self.coin = coin
-       getCounImage()
+        self.imageName = coin.id
+        getCoinImage()
     }
     
-    private func getCounImage(){
-        print("download image")
+    private func getCoinImage(){
+        if let savedIimage = fileManager.getImage(imageName: imageName, folederName: folderName){
+            image = savedIimage
+        }else{
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage(){
+        
         guard let url = URL(string: coin.image) else {return}
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handlingCompletion, receiveValue: { [weak self] (returnImage) in
-                self?.image = returnImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnImage else {return}
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
