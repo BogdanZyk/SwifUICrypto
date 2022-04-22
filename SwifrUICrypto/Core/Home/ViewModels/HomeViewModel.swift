604e6b8b215eb638690f7bc832dd4735431f64c1
Bogdan <zykovbogdan07@gmail.com>
2022-04-22 22:23:30 +0300

    adding search bar  view component

--- SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwifrUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,7 +12,7 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
-    
+    @Published var searchText: String = ""
     private let dataSevice = CounDataService()
     private var cancellables = Set<AnyCancellable>()
     
